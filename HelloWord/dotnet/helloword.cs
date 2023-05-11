ad859cc5c055eb9e6bded2311d6f4c24da0028fe
jatuncar <javier.atuncar@gmail.com>
2023-05-10 20:59:44 -0500

    qqq

--- HelloWord/dotnet/helloword.cs
+++ HelloWord/dotnet/helloword.cs
@@ -26,7 +26,7 @@ namespace Company.Function
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "yyyThis HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "fffff HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
